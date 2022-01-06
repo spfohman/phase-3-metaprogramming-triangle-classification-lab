2aa8e7578cba9c7cdbd37d5e6bdd0cb7a26af0b8
+ <+>
2022-01-06 10:09:25 -0500

    commit tree c8046f3171c6c9ede937f7e1b66fd5c5f918b31b

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,3 +1,35 @@
 class Triangle
   # write code here
+  attr_reader :side1, :side2, :side3
+
+  def initialize(side1, side2, side3)
+    @side1 = side1
+    @side2 = side2
+    @side3 = side3
+  end
+
+  def kind
+    if ([side1, side2, side3].all?(&:positive?))&&(side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1)
+
+      if side1 == side2 && side2 == side3
+        :equilateral
+      elsif side1 == side2 || side2 == side3 || side1 == side3
+        :isosceles
+      else
+        :scalene
+      end
+   
+   else
+    begin
+      raise TriangleError
+    end
+     
+   end
+  end
+
+  class TriangleError < StandardError
+    def message 
+      "That does not make a triangle"
+    end
+  end
 end
